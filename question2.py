a2dca083a990f488e710fed96f65db3b05e35312
AtiaAbk <atiasanjida1209@gmail.com>
2026-05-17 20:50:55 +0600

    question2

--- question2.py
+++ question2.py
@@ -1,6 +1,13 @@
 def attempt_task():
     for attempt in range(1, 4):
         result = input(f"attempt {attempt} - enter 'success' or 'fail': ")
+
+        try:
+            if result.strip().lower() not in ["success", "fail"]:
+                raise ValueError("Invalid input. Please enter 'success' or 'fail'.")
+        except ValueError as e:
+            print(e)
+            continue
         if result.strip().lower() == "success":
             return f"Task completed "
     return "task failed."
